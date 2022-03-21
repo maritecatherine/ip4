6ff77f219a69c94b23b4ed93d04f5d68cf8317a5
Catherine <maritecatherine91@gmail.com>
2022-03-21 22:09:57 +0300

    correct errors

--- js/script.js
+++ js/script.js
@@ -11,13 +11,16 @@ $(document).ready(function(){
 
     $(".choosen").hide();
     $("table").hide();
-    $(".extra").hide();
+    $(".extra-buttons").hide();
     $(".bill").hide();
+    $("btn.yes").hide();
+    $("btn.no").hide();
 
     $(".delivery-info").hide();
-
+    
 
     $(".btn.order").click(function(event){
+        event.preventDefault();
         var pizzaName = $(".name option:selected").val();
         var pizzaSize = $(".size option:selected").val();
         var pizzaTopping = $(".topping option:selected").val();
@@ -26,25 +29,23 @@ $(document).ready(function(){
         var unit = 1;
         var checkoutTotal = 0;
 
-    $(".choosen").show();
     $("table").show();
-    $(".extra").show();
+    $(".choosen").show();
+    $(".extra-buttons").show();
     $(".btn.order").hide();
-    $(".btn.proceed").show()
+    
+    
+    
 
-
-    $("#name").html($(".name option:selected").text() + " -" +pizzaName);
+    $("#name").html($(".name option:selected").text());
     $("#size").html($(".size option:selected").text() + "-" +pizzaSize);
     $("#crust").html($(".crust option:selected").text()+ "-" +pizzaCrust);
     $("#topping").html($(".topping option:selected").text() + "-" +pizzaTopping);
     $("#total").html(total);
 
-
-
-
-
         
-    $(".add-pizza").click(function(){
+    $(".add-pizza").click(function(event){
+        event.preventDefault();
         var pizzaName = $(".name option:selected").val();
         var pizzaSize = $(".size option:selected").val();
         var pizzaTopping = $(".topping option:selected").val();
@@ -55,38 +56,50 @@ $(document).ready(function(){
 
         var newPizza = new Pizza(pizzaName, pizzaSize, pizzaTopping, pizzaCrust, total, unit);
         
-         $("#ordersmade").append('<tr><th scope="row">' + newPizza.unit + '</th><td id="name">' + newPizza.name + '</td><td id="size">' + newPizza.size + '</td><td id="crust">' + newPizza.crust + '</td><td id="topping">'+ newPizza.topping +'</td><td id="total">' + newPizza.total +'</td></tr>'); 
+         $("#ordersmade").append('<tr><th scope="row">' + newPizza.unit + '</th><td id="name">' + newPizza.name + '</td><td id="size">' + $(".size option:selected").text() + " - "  + newPizza.size + '</td><td id="crust">'  + $(".crust option:selected").text() + " - "  + newPizza.crust + '</td><td id="topping">' + $(".topping option:selected").text() + " - "  + newPizza.topping +'</td><td id="total">' + newPizza.total +'</td></tr>'); 
          console.log(newPizza);
 
     });
-    $(".checkout").click(function(){
+    $(".btn.checkout").click(function(event){
+        event.preventDefault();
         $(".add-pizza").hide();
-        $(".checkout").hide();
+        $(".btn.checkout").hide();
         $(".bill").show();
-        $(".delivery").show();
-        $(".bill.person").hide();
+        $(".btn.yes").show();
+        $(".btn.no").show();
+        $(".person").hide();
         checkoutTotal = checkoutTotal + total;
 
-        $(".checkout-total").html(checkoutTotal);
+        $(".bill.checkout-total").html(checkoutTotal);
     });
 
-    $(".delivery").click(function(){
+    $(".btn.yes").click(function(){
         $(".delivery-cost").hide();
-        $(".delivery").hide();
+        $(".btn.yes").hide();
+        $(".btn.no").hide();
+        $(".btn.checkout").show;
         $(".person").show();
         $(".checkout-total span").html(checkoutTotal + 150);
 
     });
-    $(".proceed").click(function(){
+    $(".btn.yes").click(function(){
+        $(".delivery-cost").hide();
+        $(".btn.yes").hide();
+        $(".btn.no").hide();
+        $(".person").show();
+    });    
+
+    $(".btn.proceed").click(function(event){
+        event.preventDefault();
         var personName = $("input#name").val();
         var phone = $("input#phone").val();
-        var location = $("input#location").val();
+        var location = $(".bill#location input").val();
         $(".delivery-info").show();
         $(".person").hide();
-        $(".delivery-info span").html(location)
+        $(".delivery-info span").html(location);
 
     });
-    event.preventDefault();
+    
 
 
     });
